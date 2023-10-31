65ef6cbc447309debb2f589fd3eda0171f875375
Maxwell <njoromax5@gmail.com>
2023-10-31 15:29:24 +0300

    assignment

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
-// Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+// // Defining text characters for the empty and full hearts for you to use later.
+// const EMPTY_HEART = '♡'
+// const FULL_HEART = '♥'
 
-// Your JavaScript code goes here!
-
-
+// // Your JavaScript code goes here!
 
 
-//------------------------------------------------------------------------------
-// Don't change the code below: this function mocks the server response
-//------------------------------------------------------------------------------
+
+
+// //------------------------------------------------------------------------------
+// // Don't change the code below: this function mocks the server response
+// //------------------------------------------------------------------------------
 
 function mimicServerCall(url="http://mimicServer.example.com", config={}) {
   return new Promise(function(resolve, reject) {
@@ -19,6 +19,49 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
         reject("Random server error. Try again.");
       } else {
         resolve("Pretend remote server notified of action!");
+      }
+    }, 300);
+  });
+}
+document.addEventListener('DOMContentLoaded', function () {
+  const EMPTY_HEART = '♡';
+  const FULL_HEART = '♥';
+
+  const hearts = document.querySelectorAll('.like-glyph');
+  hearts.forEach(heart => {
+    heart.addEventListener('click', function () {
+      if (heart.textContent === EMPTY_HEART) {
+        mimicServerCall()
+          .then(() => {
+            heart.textContent = FULL_HEART;
+            heart.classList.add('activated-heart');
+          })
+          .catch(error => {
+            const errorModal = document.getElementById('error-modal');
+            errorModal.classList.remove('hidden');
+            const errorMessage = document.getElementById('modal-message');
+            errorMessage.textContent = error;
+            setTimeout(() => {
+              errorModal.classList.add('hidden');
+            }, 5000);
+          });
+      } else {
+        heart.textContent = EMPTY_HEART;
+        heart.classList.remove('activated-heart');
+      }
+    });
+  });
+});
+
+// Don't change the code below: this function mocks the server response
+function mimicServerCall(url = 'http://mimicServer.example.com', config = {}) {
+  return new Promise(function (resolve, reject) {
+    setTimeout(function () {
+      let isRandomFailure = Math.random() < 0.2;
+      if (isRandomFailure) {
+        reject('Random server error. Try again.');
+      } else {
+        resolve('Pretend remote server notified of action!');
       }
     }, 300);
   });
